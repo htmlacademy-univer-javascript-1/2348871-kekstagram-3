512d87848dc7ab69f9feea241ee08eaf74831a19
Юлия <Frolova.Y.M@mail.ru>
2023-05-08 03:11:24 +0300

    Added functions createObject, createArrayObjects

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
 function randomNumber(min, max) {
-  let minimumNumber = Math.ceil(Math.abs(min));
-  let maximumNumber = Math.floor(Math.abs(max));
-  if (minimumNumber > maximumNumber) {
-    const movement = minimumNumber;
-    minimumNumber = maximumNumber;
-    maximumNumber = movement;
-  }
-  return Math.floor(Math.random() * (maximumNumber - minimumNumber + 1) + minimumNumber);
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
 }
+
+
 function checkLengthStr(string, maxLength) {
   return string.length <= maxLength;
 
 }
 
-randomNumber(8, 1);
+const createObject = (number) => ({
+  id: number,
+  url: `photos/${number}.jpg`,
+  description: `This is my ${number} post`,
+  likes: randomNumber(15, 200),
+  comments: randomNumber(0, 200)
+});
+
+function createArrayObjects (number) {
+  const arrayOfObjects = [];
+  for (let i = 0; i < number; i++) {
+    arrayOfObjects.push(createObject(i+1));
+  }
+  return arrayOfObjects;
+}
+
 checkLengthStr('Скоро экзамены', 25);
+createArrayObjects(25);
