b9692c719fbf5103a80017f35abedb7c27fd3675
Юлия <Frolova.Y.M@mail.ru>
2023-05-08 23:42:11 +0300

    Убрала console.log

--- js/main.js
+++ js/main.js
@@ -2,4 +2,4 @@ import { createArrayObjects } from './data.js';
 import { checkLengthStr } from './util.js';
 
 checkLengthStr('Скоро экзамены', 10);
-console.log(createArrayObjects(25));
+createArrayObjects(25);
