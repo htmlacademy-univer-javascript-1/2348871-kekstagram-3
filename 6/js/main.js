bb7f81de8e4fec3a88bf63da7f8c6bc610e3365c
Keks <keks@htmlacademy.ru>
2023-05-13 17:52:33 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,6 @@
+import { createArrayObjects } from './data.js';
+import { checkLengthStr } from './util.js';
+import '/js/popup.js';
+
+checkLengthStr('Скоро экзамены', 10);
+createArrayObjects(25);
